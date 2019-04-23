7ca6b16f287940ce54e659db347095ce20a30d20
Anonymous <ivorantos@alumnos.unex.es>
2019-04-24 01:16:35 +0200

    Cambio en los paneles

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,8 +10,11 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
+
+//        primaryStage.setMaximized(true);//abrir en pantalla completa
         primaryStage.setScene(new Scene(root, 600, 600));
         primaryStage.show();
     }
